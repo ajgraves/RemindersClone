5a0304b333cf519313bf8854791cd2404aeb7ac5
Aaron Graves <ajgraves@ajgraves.com>
2024-08-22 23:15:05 -0500

    Initial Commit

---
+++ RemindersClone/RemindersCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersCloneApp.swift
+//  RemindersClone
+//
+//  Created by Aaron Graves on 8/22/24.
+//
+
+import SwiftUI
+
+@main
+struct RemindersCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
